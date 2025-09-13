f7df9bc96ed3a6cf2afb4aa1c464df0685f254a8
Said Al Ghiyats <said.ghiyatss@gmail.com>
2025-09-14 01:11:39 +0700

    refactor(header): make logo clickable to navigate to home page
    Update the header component to wrap the logo in a Link component, allowing users to navigate back to the home page by clicking on it

--- components/header.tsx
+++ components/header.tsx
@@ -2,15 +2,16 @@
 
 import Image from "next/image";
 import { ThemeSwitcher } from "./theme-switcher";
+import Link from "next/link";
 
 export function Header() {
   return (
     <header className="border-b border-border bg-card/50 backdrop-blur-sm sticky top-0 z-50">
       <div className="container mx-auto px-12 py-4">
         <div className="flex items-center justify-between">
-          <div className="flex items-center space-x-4">
+          <Link href="/" className="flex items-center space-x-4">
             <Image src="/logo-ptq.svg" width={32} height={32} alt="ptq-logo" />
-          </div>
+          </Link>
 
           <div className="flex items-center space-x-2">
             <ThemeSwitcher />
